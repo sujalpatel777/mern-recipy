9b4bcc39afc30d0ce983cc02f3d8b2c6631687c0
sujalkumar kishorbhai patel <142832374+sujalpatel777@users.noreply.github.com>
2025-07-17 23:30:35 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,10 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: ["https://mern-recipy-1.onrender.com", "http://localhost:5173"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
